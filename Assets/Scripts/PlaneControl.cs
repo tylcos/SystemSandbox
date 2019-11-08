81c272ccf96775ee129572142533ae50d412548d
Tyler Cosentino <tylcos@users.noreply.github.com>
2019-11-08 16:55:01 -0500

    Added custom skybox to ocean scene and started main menu UI
    Former-commit-id: 9eab3baf2321a76daed8a56882dab9d68e7d738c
    
    Former-commit-id: 8fab0f31690c52bc311886ac4c108b259bc5e107
    
    Former-commit-id: ee5a53d910b10e7b402a1ce2d433bc6c7acfc5e5

--- Assets/Scripts/PlaneControl.cs
+++ Assets/Scripts/PlaneControl.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using UnityEngine;
@@ -9,20 +9,32 @@ class PlaneControl : MonoBehaviour
     public float speed = 10f;
     public float accel = 10f;
     public Rigidbody rb;
+    public Camera camera;
+
+    public float[] times;
 
 
 
     private void Start()
     {
         rb.velocity = transform.forward * speed;
+
+        StartCoroutine(ChangeSkyBox());
     }
-
-
 
     private void FixedUpdate()
     {
         transform.rotation *= Quaternion.Euler(-2 * Time.fixedDeltaTime, 0, 0);
         rb.velocity = transform.forward * rb.velocity.magnitude;
         rb.AddForce(transform.forward * accel * Time.fixedDeltaTime, ForceMode.Acceleration);
+    }
+
+
+    private IEnumerator ChangeSkyBox()
+    {
+        yield return new WaitForSeconds(times[0]);
+
+        print("Color");
+        camera.backgroundColor = UnityEngine.Random.ColorHSV();
     }
 }
---
+++ Assets/Text.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    internal int fontSize;
+    internal TextAnchor alignment;
+    internal string text;
+    internal Font font;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
