bc4b811d6aef3bb6df0a7d8cdfec268b4e0cdaa4
tylcos <tylcos@users.noreply.github.com>
2020-08-19 02:15:53 -0400

    Old metric spaces test

---
+++ Assets/MetricSpace.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using MathNet.Numerics.LinearAlgebra;
+
+public static class MetricSpace
+{
+    static Vector<float> Metric(Vector2 pos) => Vector<float>.Build.Dense(new float[] { pos.x * pos.x + 1, 1000f }).PointwiseAbs();
+
+    public static Vector2 DeltaPos(Vector2 pos, Vector2 vel)
+    {
+        var deltaPos = Convert(vel * Time.deltaTime);
+        var deltaPosMetric = deltaPos.PointwiseMultiply(Metric(pos));
+
+        var deltaPosRatio = deltaPos.PointwiseDivide(deltaPosMetric);
+        var trueDeltaPos = deltaPosRatio.PointwiseMultiply(deltaPos);
+
+        return Convert(trueDeltaPos);
+    }
+
+
+
+    public static Vector<float> Convert(Vector2 v) => Vector<float>.Build.Dense(new float[] { v.x, v.y });
+    public static Vector2 Convert(Vector<float> v) => new Vector2(v[0], v[1]);
+}
---
+++ Assets/TestObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TestObject : MonoBehaviour
+{
+    private readonly Vector2 Velocity = new Vector2(20f, 1f);
+
+
+
+    void Update()
+    {
+        transform.position += (Vector3)MetricSpace.DeltaPos(transform.position, Velocity);
+    }
+}
